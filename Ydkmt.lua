98a2e3a24551d4727b7a1c4c51969320dd9663dc
yaD5 <56451679+yaD5@users.noreply.github.com>
2024-01-03 06:15:29 +0300

    Update Ydkmt.lua

--- Ydkmt.lua
+++ Ydkmt.lua
@@ -225,12 +225,12 @@ end)
 
 local function jumpToServer() 
     local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
-    local req = request({ Url = string.format(sfUrl, 15588442388, "Desc", 100) }) 
+    local req = request({ Url = string.format(sfUrl, 15502339080, "Desc", 100) }) 
     local body = http:JSONDecode(req.Body) 
     local deep = math.random(1, 3)
     if deep > 1 then 
         for i = 1, deep, 1 do 
-             req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 15588442388, "Desc", 100) }) 
+             req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 15502339080, "Desc", 100) }) 
              body = http:JSONDecode(req.Body) 
              task.wait(0.1)
         end 
@@ -247,7 +247,7 @@ local function jumpToServer()
     if not randomCount then
        randomCount = 2
     end
-    ts:TeleportToPlaceInstance(15588442388, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer) 
+    ts:TeleportToPlaceInstance(15502339080, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer) 
 end
 
 Players.PlayerRemoving:Connect(function(player)
