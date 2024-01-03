9878d44420229c3467caab2cb112165e4ab022e5
yaD5 <56451679+yaD5@users.noreply.github.com>
2024-01-03 16:16:09 +0300

    Update Ydkmt.lua

--- Ydkmt.lua
+++ Ydkmt.lua
@@ -144,7 +144,7 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
     end
 
     local price = gems / amount
-    task.wait(3.12)
+    task.wait(3.05)
     if type.huge and price <= 1000000 then	
         local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
         if boughtPet == true then
